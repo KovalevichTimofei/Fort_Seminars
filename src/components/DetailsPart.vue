c74112ae950631b4f490d3e720a69ff8aab7f853
Tsimafei Kavalevich <Tsimafei.Kavalevich@itechart-group.com>
2019-10-01 10:01:56 +0300

    fix initial month number

--- src/components/DetailsPart.vue
+++ src/components/DetailsPart.vue
@@ -24,7 +24,7 @@
     components: { SectionTitle },
     data: function() {
           return {
-          monthNumber: 0,
+          monthNumber: (new Date().getMonth() + 1),
         }
     },
     methods: {
